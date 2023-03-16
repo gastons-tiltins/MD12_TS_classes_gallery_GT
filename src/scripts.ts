2ef08401d68e094e0811a5a7faaa43da5a450763
Gastons Tiltins <gastons.tiltins@gmail.com>
2023-03-16 04:44:33 +0200

    updated types

--- src/scripts.ts
+++ src/scripts.ts
@@ -2,30 +2,30 @@
 let cardAmount = 5;
 
 class ImagesGallery {
-    i: number;
-    sIndex: number = 1;
-    timer: any;
+  i: number;
+  sIndex: number = 1;
+  timer: ReturnType<typeof setTimeout> | undefined;
 
-    constructor(i: number) {
-        this.i = i;
+  constructor(i: number) {
+    this.i = i;
 
-        // Generate all cards
-        for (let i = 1; i <= cardAmount - 2; i++) {
-            this.makeCard(i);
-        }
-
-        // Add arrow button events
-        this.arrowButton('.prev', -1);
-        this.arrowButton('.next', 1);
-
-        // State on page load
-        this.slidesLogic(1);
+    // Generate all cards
+    for (let i = 1; i <= cardAmount - 2; i++) {
+      this.makeCard(i);
     }
 
-    // Generate single card
-    makeCard = (i: number) => {
-        let cardDiv = document.createElement('div');
-        cardDiv.innerHTML = `
+    // Add arrow button events
+    this.arrowButton('.prev', -1);
+    this.arrowButton('.next', 1);
+
+    // State on page load
+    this.slidesLogic(1);
+  }
+
+  // Generate single card
+  makeCard = (i: number) => {
+    let cardDiv = document.createElement('div');
+    cardDiv.innerHTML = `
         <div class="slides my-fade">
             <img
                 src="./assets/images/img${i}.jpg"
@@ -35,107 +35,109 @@ class ImagesGallery {
             />
         </div>
         `;
-        document.getElementsByClassName('cars-frame')[0].appendChild(cardDiv);
-    };
+    document.getElementsByClassName('cars-frame')[0].appendChild(cardDiv);
+  };
 
-    // Arrow button even function
-    arrowButton = (className: string, n: number) => {
-        const cn = document.querySelector(className);
-        cn.addEventListener('click', () => {
-            this.nextSlide(n);
-        });
-    };
+  // Arrow button even function
+  arrowButton = (className: string, n: number) => {
+    const cn = document.querySelector(className);
+    cn.addEventListener('click', () => {
+      this.nextSlide(n);
+    });
+  };
 
-    // Button logic
+  // Button logic
 
-    nextSlide = (n: number) => {
-        this.slidesLogic((this.sIndex += n));
-    };
+  nextSlide = (n: number) => {
+    this.slidesLogic((this.sIndex += n));
+  };
 
-    currentSlide = (n: number) => {
-        this.slidesLogic((this.sIndex = n));
-    };
+  currentSlide = (n: number) => {
+    this.slidesLogic((this.sIndex = n));
+  };
 
-    slidesLogic(n: number) {
-        let i;
-        let slides: any = document.getElementsByClassName('slides');
-        let circles: any = document.getElementsByClassName('circle');
-        if (n > slides.length) {
-            this.sIndex = 1;
-        }
-        if (n < 1) {
-            this.sIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        for (i = 0; i < circles.length; i++) {
-            circles[i].className = circles[i].className.replace('active', '');
-        }
-        slides[this.sIndex - 1].style.display = 'block';
+  slidesLogic(n: number) {
+    let i;
+    let slides: HTMLCollectionOf<Element> =
+      document.getElementsByClassName('slides');
+    let circles: HTMLCollectionOf<Element> =
+      document.getElementsByClassName('circle');
+    if (n > slides.length) {
+      this.sIndex = 1;
     }
+    if (n < 1) {
+      this.sIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      (slides[i] as HTMLElement).style.display = 'none';
+    }
+    for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace('active', '');
+    }
+    (slides[this.sIndex - 1] as HTMLElement).style.display = 'block';
+  }
 }
 
 class AddRoundButtons extends ImagesGallery {
-    i: number;
+  i: number;
 
-    constructor(i: number) {
-        super(cardAmount);
-        // Generate all circle buttons
-        for (let i = 1; i <= cardAmount - 2; i++) {
-            this.circleButton(i);
-        }
-        // Add circle button events
-        for (let i = 1; i <= cardAmount - 2; i++) {
-            this.circleButtonEvent(`.circle${i}`, i);
-        }
+  constructor(i: number) {
+    super(cardAmount);
+    // Generate all circle buttons
+    for (let i = 1; i <= cardAmount - 2; i++) {
+      this.circleButton(i);
     }
+    // Add circle button events
+    for (let i = 1; i <= cardAmount - 2; i++) {
+      this.circleButtonEvent(`.circle${i}`, i);
+    }
+  }
 
-    // make Circle buttons
-    circleButton = (i: number) => {
-        let cardDiv = document.createElement('div');
-        cardDiv.innerHTML = `
+  // make Circle buttons
+  circleButton = (i: number) => {
+    let cardDiv = document.createElement('div');
+    cardDiv.innerHTML = `
             <div class="center-dots">
                 <span class="circle circle${i}"></span>
             </div>
         `;
-        document.getElementsByClassName('center-dots')[0].appendChild(cardDiv);
-    };
+    document.getElementsByClassName('center-dots')[0].appendChild(cardDiv);
+  };
 
-    // Circle button event function
-    circleButtonEvent = (className: string, n: number) => {
-        const cn = document.querySelector(className);
-        cn.addEventListener('click', () => {
-            this.currentSlide(n);
-        });
-    };
+  // Circle button event function
+  circleButtonEvent = (className: string, n: number) => {
+    const cn = document.querySelector(className);
+    cn.addEventListener('click', () => {
+      this.currentSlide(n);
+    });
+  };
 }
 
 class Thumbnails extends AddRoundButtons {
-    i: number;
-    constructor(i: number) {
-        super(cardAmount);
+  i: number;
+  constructor(i: number) {
+    super(cardAmount);
 
-        // add two cards
-        for (i = 4; i <= 5; i++) {
-            this.makeCard(i);
-            this.circleButton(i);
-            this.circleButtonEvent(`.circle${i}`, i);
-        }
-
-        // Generate all thumbnail images
-        for (let i = 1; i <= cardAmount; i++) {
-            this.makeThumbnails(i);
-        }
-        // Generate thumbnail events
-        for (let i = 1; i <= cardAmount; i++) {
-            this.ThumbnailsImagesEvent(`.thumbnail${i}`, i);
-        }
+    // add two cards
+    for (i = 4; i <= 5; i++) {
+      this.makeCard(i);
+      this.circleButton(i);
+      this.circleButtonEvent(`.circle${i}`, i);
     }
-    // make Thumbnails
-    makeThumbnails = (i: number) => {
-        let thumbDiv = document.createElement('row');
-        thumbDiv.innerHTML = `
+
+    // Generate all thumbnail images
+    for (let i = 1; i <= cardAmount; i++) {
+      this.makeThumbnails(i);
+    }
+    // Generate thumbnail events
+    for (let i = 1; i <= cardAmount; i++) {
+      this.ThumbnailsImagesEvent(`.thumbnail${i}`, i);
+    }
+  }
+  // make Thumbnails
+  makeThumbnails = (i: number) => {
+    let thumbDiv = document.createElement('row');
+    thumbDiv.innerHTML = `
         <div class="column">
             <img
                 class="thumbnail${i} demo cursor"
@@ -144,60 +146,60 @@ class Thumbnails extends AddRoundButtons {
             />
         </div>
     `;
-        document.getElementsByClassName('row')[0].appendChild(thumbDiv);
-    };
+    document.getElementsByClassName('row')[0].appendChild(thumbDiv);
+  };
 
-    // Thumbnail images event function
-    ThumbnailsImagesEvent = (className: string, n: number) => {
-        const cn = document.querySelector(className);
-        cn.addEventListener('click', () => {
-            this.currentSlide(n);
-        });
-    };
+  // Thumbnail images event function
+  ThumbnailsImagesEvent = (className: string, n: number) => {
+    const cn = document.querySelector(className);
+    cn.addEventListener('click', () => {
+      this.currentSlide(n);
+    });
+  };
 }
 
 class FullScreen extends Thumbnails {
-    i: number;
-    constructor(i: number) {
-        super(cardAmount);
+  i: number;
+  constructor(i: number) {
+    super(cardAmount);
 
-        // Create fullscreen events
-        for (let i = 1; i <= cardAmount; i++) {
-            this.fullScreen(i);
-        }
-
-        // add pointer over slides
-        for (let i = 1; i <= cardAmount; i++) {
-            document.getElementById(`car${i}`).style.cursor = 'pointer';
-        }
+    // Create fullscreen events
+    for (let i = 1; i <= cardAmount; i++) {
+      this.fullScreen(i);
     }
-    fullScreen = (i: number) => {
-        let modal = document.getElementById('full-screen');
-        let img: any = document.getElementById(`car${i}`);
-        let modalImg: any = document.getElementById('f-image');
-        img.onclick = function () {
-            modal.style.display = 'block';
-            modalImg.src = this.src;
-        };
-        let span: any = document.getElementsByClassName('close')[0];
-        span.onclick = function () {
-            modal.style.display = 'none';
-        };
-        document.addEventListener('keydown', (evt) => {
-            if (evt.key === 'Escape') {
-                modal.style.display = 'none';
-            }
-        });
+
+    // add pointer over slides
+    for (let i = 1; i <= cardAmount; i++) {
+      document.getElementById(`car${i}`).style.cursor = 'pointer';
+    }
+  }
+  fullScreen = (i: number) => {
+    let modal = document.getElementById('full-screen') as HTMLElement;
+    let img = document.getElementById(`car${i}`) as HTMLImageElement | null;
+    let modalImg: any = document.getElementById('f-image') as HTMLImageElement;
+    img.onclick = function (this: HTMLImageElement) {
+      modal.style.display = 'block';
+      modalImg.src = this.src;
     };
+    let span: any = document.getElementsByClassName('close')[0];
+    span.onclick = function () {
+      modal.style.display = 'none';
+    };
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        modal.style.display = 'none';
+      }
+    });
+  };
 
-    // Extend Parrent class method
-    slidesLogic(n: number) {
-        super.slidesLogic(n);
+  // Extend Parrent class method
+  slidesLogic(n: number) {
+    super.slidesLogic(n);
 
-        // timed gallery event
-        clearTimeout(this.timer);
-        this.timer = setTimeout(() => this.nextSlide(1), 10000);
-    }
+    // timed gallery event
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.nextSlide(1), 10000);
+  }
 }
 
 // ++++++++++++++++++++++++++++++++++++++++++
